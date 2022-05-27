34609ffd484951b36e7d57aaf5c1e7ac3efad9fa
Angel Soto <100176750+Angeltheesoto@users.noreply.github.com>
2022-05-26 20:05:58 -0400

    Finished Basics section

--- 1 - Basics/index.js
+++ 1 - Basics/index.js
@@ -127,14 +127,14 @@ camel case or underscore
 // Arrays [] -, 0 index based
 
 // array
-const friends = [
- 'Bob',
- 'Anna',
- 'Susy',
- // 10,
- // null,
- // undefined
-];
+// const friends = [
+//  'Bob',
+//  'Anna',
+//  'Susy',
+//  // 10,
+//  // null,
+//  // undefined
+// ];
 
 // let bestFriend = friends[2];
 // friends[4] = 'anna'
@@ -183,20 +183,130 @@ const friends = [
 
 // Below we want to create a function that changes inch to centemeters = 1 inch = 2.54cm
 
-const wallHeight = 80;
+// const wallHeight = 80;
 
-function calculate(value) {
- console.log('The value in cm is : ' + value * 2.54 + ' cm')
+// function calculate(value) {
+//  console.log('The value in cm is : ' + value * 2.54 + ' cm')
+
  // const newValue = value * 2.54
  // return newValue // or just return the equation
- return value * 2.54
-};
 
-const width = calculate(100);
-const height = calculate(wallHeight);
+//  return value * 2.54
+// };
 
-const dimensions = [width, height]; 
-console.log(dimensions); // this returns undefined because there is no return value in the function above.
+// const width = calculate(100);
+// const height = calculate(wallHeight);
+
+// const dimensions = [width, height]; 
+// console.log(dimensions); // this returns undefined because there is no return value in the function above.
 
 
-// Expressions
+// Expressions/ Functions -
+// expressions is another wat to define a function.
+// function addValues(num1, num2) {
+//  return num1 + num2;
+// }
+
+// const firstValue = addValues(3, 4);
+// const secondValue = addValues(12, 34);
+// // function expression
+// const add = function addValues(num1, num2) {
+//  return num1 + num2;
+// }
+// // const thirdValue = add(5, 6)
+// const values = [firstValue, secondValue, add(5, 6)];
+// console.log(values);
+
+// const multiply = (num1, num2) => num1 * num2;
+
+
+// Objects -
+// use key/ value pairs methods. Dot notation
+// const person = {
+//  name: 'Angel Soto',
+//  age: 24,
+//  'Eye Color': 'Brown',
+//  married: false,
+//  siblings: ['Max Santos', null],
+//  greeting: (para, para2) => {
+//   return `${para} ${para2}`
+//  }
+// };
+
+// console.log(person.greeting(person.name, person.age))
+
+
+// conditional Statements
+// >, <, >=, <=, ==, ===, !=, !==
+// if(1  === 1) {
+//  console.log(true)
+// }
+// //
+// if(1 === '1') {
+//  console.log(true)
+// } else {
+//  console.log(false)
+// }
+// //
+// const num1 = 6;
+// const num2 = 6;
+// const result = num1 == num2;
+// if(num1 > num2) {
+//  console.log('First number is bigger than second number.')
+// } else if(result) {
+//  console.log('Both are equal to eachother.')
+// } else {
+//  console.log('Second number is bigger than first')
+// }
+
+// Logical operator -
+// ( || - OR ), ( && - AND), !
+// const name = 'Angel';
+// const age = 24;
+
+// if(name === 'bob' && age === 24) {
+//  console.log('hello there user');
+// } else if(name === 'Angel' && age === 24) {
+//  console.log('wrong values');
+// }
+
+// switch statements
+// dice values: 1 - 6
+// const dice = 2;
+
+// switch (dice) {
+//  case 1:
+//   console.log('You got one.');
+//   break;
+//  case 2:
+//   console.log('You got two.');
+//   break;
+
+//  default:
+//   break;
+// }
+
+
+// LOOPS ----
+
+// While loop - Repeatedly run a block of code while the condition is true.
+// let amount = 10;
+// while (amount > 0) {
+//  // console.log('I have ' + amount + ' dollars and im going to the mall');
+//  console.log(`I have ${amount} dollars and im going to the mall`);
+//  amount--;
+// };
+
+// Do While loop - Repeatedly run a block of code while a condition is true.
+// code block first, condition second
+// let money = 0;
+// do{
+//  console.log('You have ' + money + ' dollars');
+//  money++;
+// }
+// while(money < 10)
+
+// For Loop - repeatedly runs a block of code while condition is true.
+// for(let i = 10; i >= 0; i--){
+//  console.log('countdown: ' + i)
+// };
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Javascript-Fundamentals
+Javascript-Fundamentals
+
+In this intermediate section I will go over and review a little bit more of complex fundamentals of javascript.
